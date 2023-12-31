13bf934874ebf03ce95c8004816d4dcae384123c
Gayatri-Khairnar <147025807+Gayatri-Khairnar@users.noreply.github.com>
2023-12-31 18:54:20 +0530

    Delete src/main/java/com/agro directory

--- src/main/java/com/agro/repository/UserRoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agro.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agro.entity.UserRole;
-
-public interface UserRoleRepository extends JpaRepository<UserRole, Integer>{
-
-}
